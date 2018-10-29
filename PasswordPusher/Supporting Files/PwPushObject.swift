7e15d98b3ef955d3ab309af5cc40a4e41dce2ebb
Brian Ogilvie <brian.ogilvie@arctouch.com>
2018-10-29 15:21:39 -0400

    removed commented code for current pwpush api

--- PasswordPusher/Supporting Files/PwPushObject.swift
+++ PasswordPusher/Supporting Files/PwPushObject.swift
@@ -10,12 +10,10 @@ import Foundation
 
 struct PwPushObject: Decodable {
     var created: String
-    //var deletable: Bool
     var deleted: Bool
     var expireDays: Int
     var expireViews: Int
     var expired: Bool
-    //var firstView: Bool
     var id: Int
     var payload: String
     var updatedAt: String
@@ -24,12 +22,10 @@ struct PwPushObject: Decodable {
     
     enum CodingKeys: String, CodingKey {
         case created = "created_at"
-        //case deletable = "deletable_by_viewer"
         case deleted
         case expireDays = "expire_after_days"
         case expireViews = "expire_after_views"
         case expired
-        //case firstView = "first_view"
         case id
         case payload
         case updatedAt = "updated_at"
@@ -37,19 +33,3 @@ struct PwPushObject: Decodable {
         case userId = "user_id"
     }
 }
-
-//Older API hosted at arctouch with limited fields
-/*
- jsonData: {
- "created_at" = "2018-10-26T18:55:36Z";
- deleted = 0;
- "expire_after_days" = 7;
- "expire_after_views" = 5;
- expired = 0;
- id = 2379;
- payload = "GHMAa6zAdbNPKGAfN7WSIg==\n";
- "updated_at" = "2018-10-26T18:55:36Z";
- "url_token" = h5720tztda19y8zr;
- "user_id" = "<null>";
- }
- */
