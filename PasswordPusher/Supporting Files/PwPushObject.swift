a6e898b0bad0a1f8fe904fdf82f3fb76d71bdd12
Pedro van Rooij Costa <pedro.costa@arctouch.com>
2018-11-06 17:15:27 -0800

    Rebasing branch and add files back on to it

---
+++ PasswordPusher/Supporting Files/PwPushObject.swift
@@ -0,0 +1,35 @@
+//
+//  PwPushObject.swift
+//  PasswordPusher
+//
+//  Created by Brian Ogilvie on 10/8/18.
+//  Copyright © 2018 Brian Ogilvie Development. All rights reserved.
+//
+
+import Foundation
+
+struct PwPushObject: Decodable {
+    var created: String
+    var deleted: Bool
+    var expireDays: Int
+    var expireViews: Int
+    var expired: Bool
+    var id: Int
+    var payload: String
+    var updatedAt: String
+    var urlToken: String
+    var userId: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case created = "created_at"
+        case deleted
+        case expireDays = "expire_after_days"
+        case expireViews = "expire_after_views"
+        case expired
+        case id
+        case payload
+        case updatedAt = "updated_at"
+        case urlToken = "url_token"
+        case userId = "user_id"
+    }
+}
