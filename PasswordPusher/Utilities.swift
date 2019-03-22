0ca9f7c46703138ade21e2cf52f5cb59089679c7
Brian Ogilvie <brian.ogilvie@arctouch.com>
2019-03-22 11:50:16 -0400

    added wood background. added license files. changed page headers to reference brian ogilvie development

--- PasswordPusher/Utilities.swift
+++ PasswordPusher/Utilities.swift
@@ -2,16 +2,9 @@
 //  Utilities.swift
 //  PasswordPusher
 //
-// Copyright 2018 ArcTouch, LLC.
-// All rights reserved.
-//
-// This file, its contents, concepts, methods, behavior, and operation
-// (collectively the "Software") are protected by trade secret, patent,
-// and copyright laws. The use of the Software is governed by a license
-// agreement. Disclosure of the Software to third parties, in any form,
-// in whole or in part, is expressly prohibited except as authorized by
-// the license agreement.
-//
+//  Created by Brian Ogilvie on 3/22/19.
+//  Copyright © 2019 Brian Ogilvie Development. All rights reserved.
+
 import UIKit
 
 extension UIViewController {
---
+++ PasswordPusher/heading.swift
@@ -0,0 +1,9 @@
+//
+//  heading.swift
+//  PasswordPusher
+//
+//  Created by Brian Ogilvie on 3/22/19.
+//  Copyright © 2019 Brian Ogilvie Development. All rights reserved.
+//
+
+import Foundation
