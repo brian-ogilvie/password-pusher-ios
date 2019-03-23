7d9ff89cff6442343a496a933878a860fe21dc9e
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-23 16:28:19 -0500

    added mixpanel for analytics

--- PasswordPusher/AppDelegate.swift
+++ PasswordPusher/AppDelegate.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2019 Brian Ogilvie Development. All rights reserved.
 
 import UIKit
+import Mixpanel
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Mixpanel.initialize(token: "311498e25210cf80cc6bbe9a3be9104b")
+        return true
+    }
 }
 
